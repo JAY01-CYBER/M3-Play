3bf369b412c93ce33c13efc3e9efd20553e43050
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-22 07:37:11 +0530

    Update MainActivity.kt

--- app/src/main/java/com/j/m3play/MainActivity.kt
+++ app/src/main/java/com/j/m3play/MainActivity.kt
@@ -1596,7 +1596,7 @@ private fun openNotificationSettings(context: Context) {
 
 suspend fun checkForUpdates(): String? = withContext(Dispatchers.IO) {
     try {
-        val url = URL("https://api.github.com/repos/Arturo254/OpenTune/releases/latest")
+        val url = URL("https://api.github.com/repos/JAY01-CYBER/M3-Play/releases/latest")
         val connection = url.openConnection()
         connection.connect()
         val json = connection.getInputStream().bufferedReader().use { it.readText() }
