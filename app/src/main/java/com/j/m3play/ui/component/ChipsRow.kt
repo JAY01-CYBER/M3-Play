0a7be535a6572a4f15ab77be051a43dd0106341b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-03-30 07:26:41 +0530

    Update ChipsRow.kt

--- app/src/main/java/com/j/m3play/ui/component/ChipsRow.kt
+++ app/src/main/java/com/j/m3play/ui/component/ChipsRow.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.systemBars
 
 @Composable
 fun <E> ChipsRow(
