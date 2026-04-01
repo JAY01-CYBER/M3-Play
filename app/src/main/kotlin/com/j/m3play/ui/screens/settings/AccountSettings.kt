4cb4f91ed84ae89d0b3fbe11bd90a7188e784c29
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-04-01 17:51:37 +0530

    Update AccountSettings.kt

--- app/src/main/kotlin/com/j/m3play/ui/screens/settings/AccountSettings.kt
+++ app/src/main/kotlin/com/j/m3play/ui/screens/settings/AccountSettings.kt
@@ -354,7 +354,7 @@ private fun AccountSettingsHeader(onClose: () -> Unit) {
                     painter = painterResource(R.drawable.ic_app_logo),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(30.dp)
+                        .size(22.dp)
                 )
 
                 Text(
