b369c5b91782e4797cf24405efe7c3178aa7d228
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-04-05 09:37:12 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "com.j.m3play"
         minSdk = 26
         targetSdk = 36
-        versionCode = 134
-        versionName = "3.0.1"
+        versionCode = 135
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
