84d14fcd0da35beda374f204c3f3e226abc7b459
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-04-14 22:42:31 +0530

    Add files via upload

--- app/src/main/kotlin/com/j/m3play/MainActivity.kt
+++ app/src/main/kotlin/com/j/m3play/MainActivity.kt
@@ -321,6 +321,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
+        startMusicServiceSafely()
         isMusicServiceBound =
             bindService(
                 Intent(this, MusicService::class.java),
@@ -1737,8 +1738,14 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun startMusicServiceSafely() {
-        runCatching { startService(Intent(this, com.j.m3play.playback.MusicService::class.java)) }
-            .onFailure { reportException(it) }
+        runCatching {
+            val intent = Intent(this, com.j.m3play.playback.MusicService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+        }.onFailure { reportException(it) }
     }
 
     @SuppressLint("ObsoleteSdkInt")
