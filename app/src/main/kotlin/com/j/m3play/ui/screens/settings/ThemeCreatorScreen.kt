19dfb5c202a4abdbe00187f12abd5055ce24253b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-05-21 12:11:42 +0530

    Update ThemeCreatorScreen.kt

--- app/src/main/kotlin/com/j/m3play/ui/screens/settings/ThemeCreatorScreen.kt
+++ app/src/main/kotlin/com/j/m3play/ui/screens/settings/ThemeCreatorScreen.kt
@@ -96,6 +96,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
