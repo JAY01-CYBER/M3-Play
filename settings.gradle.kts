d476b2417107623875d7898bb70cc2709af45c96
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-05-29 09:18:16 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,6 +34,8 @@ include(":betterlyrics")
 include(":kizzy")
 include(":canvas")
 include(":shazamkit")
+include(":youlyplus")
+
 
 // Use a local copy of NewPipe Extractor by uncommenting the lines below.
 // We assume, that M3Play and NewPipe Extractor have the same parent directory.
