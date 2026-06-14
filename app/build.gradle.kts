276581e4e92208f93b9477f4d6da3732351cd9b3
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-06-14 09:01:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -248,6 +248,7 @@ dependencies {
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.okhttp)
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.11")
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.websockets)
     implementation(libs.ktor.server.core)
