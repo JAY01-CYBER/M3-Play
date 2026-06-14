1537a976867bedcbace4405feac25802cd2a744f
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-06-14 14:59:52 +0530

    Update LibraryArtistsScreen.kt

--- app/src/main/kotlin/com/j/m3play/ui/screens/library/LibraryArtistsScreen.kt
+++ app/src/main/kotlin/com/j/m3play/ui/screens/library/LibraryArtistsScreen.kt
@@ -12,11 +12,13 @@ package com.j.m3play.ui.screens.library
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -37,6 +39,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
