fe5f73f95862a1475a46311d4a767822cfba619b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-06-21 07:33:24 +0530

    Update AutoPlaylistScreen.kt

--- app/src/main/kotlin/com/j/m3play/ui/screens/playlist/AutoPlaylistScreen.kt
+++ app/src/main/kotlin/com/j/m3play/ui/screens/playlist/AutoPlaylistScreen.kt
@@ -70,6 +70,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
