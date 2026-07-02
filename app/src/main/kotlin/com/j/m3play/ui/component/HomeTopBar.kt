8db4eb668194713a0726469d81c24af19a9f206b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-07-02 09:47:08 +0530

    Add safeDrawing import to HomeTopBar.kt

--- app/src/main/kotlin/com/j/m3play/ui/component/HomeTopBar.kt
+++ app/src/main/kotlin/com/j/m3play/ui/component/HomeTopBar.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.j.m3play.R
