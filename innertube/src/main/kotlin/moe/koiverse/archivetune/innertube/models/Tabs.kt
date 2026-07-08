3cd4a1cdc258abc14d53a9c9ed586c48bb06c0d8
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-07-08 17:46:44 +0530

    Add files via upload

--- innertube/src/main/kotlin/moe/koiverse/archivetune/innertube/models/Tabs.kt
+++ innertube/src/main/kotlin/moe/koiverse/archivetune/innertube/models/Tabs.kt
@@ -11,46 +11,23 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Tabs(
-    val tabs: List<Tab>? = null
+    val tabs: List<Tab>,
 ) {
     @Serializable
     data class Tab(
-        val tabRenderer: TabRenderer? = null
+        val tabRenderer: TabRenderer,
     ) {
         @Serializable
         data class TabRenderer(
-            val title: String? = null,
-            val selected: Boolean? = null,
-            val content: Content? = null,
-            val endpoint: NavigationEndpoint? = null
+            val title: String?,
+            val content: Content?,
+            val endpoint: NavigationEndpoint?,
         ) {
             @Serializable
             data class Content(
-                val sectionListRenderer: SectionListRenderer? = null,
-                val musicQueueRenderer: MusicQueueRenderer? = null
-            ) {
-                @Serializable
-                data class SectionListRenderer(
-                    val contents: List<Content>? = null,
-                    val continuations: List<Continuation>? = null,
-                    val header: Header? = null
-                ) {
-                    @Serializable
-                    data class Content(
-                        val musicCardShelfRenderer: MusicCardShelfRenderer? = null,
-                        
-                        
-                        val musicShelfRenderer: MusicShelfRenderer? = null, 
-                        val musicCarouselShelfRenderer: MusicCarouselShelfRenderer? = null,
-                        val gridRenderer: GridRenderer? = null
-                    )
-                    
-                    @Serializable
-                    data class Header(
-                        val chipCloudRenderer: ChipCloudRenderer? = null
-                    )
-                }
-            }
+                val sectionListRenderer: SectionListRenderer?,
+                val musicQueueRenderer: MusicQueueRenderer?,
+            )
         }
     }
 }
