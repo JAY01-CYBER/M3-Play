ad23743cf98f58cab530db72861f41bcebbb0d59
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-07-14 08:03:37 +0530

    Add import for R in ThemeAwareLoadingAnimation

--- app/src/main/kotlin/com/j/m3play/ui/component/ThemeAwareLoadingAnimation.kt
+++ app/src/main/kotlin/com/j/m3play/ui/component/ThemeAwareLoadingAnimation.kt
@@ -1,6 +1,6 @@
 package com.j.m3play.ui.component
 
-
+import com.j.m3play.R
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
